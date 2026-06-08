6d2536b12738a0590dccfaf9b502d34de817a9dd
Nermin Sehic <sehicnermin97@gmail.com>
2026-06-08 18:25:15 +1000

    Use UUIDv7 for unique ID generation

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/nsehic/go-sip
 
 go 1.26.4
 
-require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20251128144731-cb7f07239012
+require (
+	github.com/google/uuid v1.6.0
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20251128144731-cb7f07239012
+)
--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"crypto/rand"
 	"encoding/json"
-	"fmt"
 	"log"
 
+	"github.com/google/uuid"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
-
-func generateUUIDv4() (string, error) {
-	uuid := make([]byte, 16)
-	_, err := rand.Read(uuid)
-	if err != nil {
-		return "", err
-	}
-
-	uuid[8] = (uuid[8] & 0x3f) | 0x80
-	uuid[6] = (uuid[6] & 0x0f) | 0x40
-
-	return fmt.Sprintf("%x-%x-%x-%x-%x",
-		uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:16]), nil
-}
 
 func main() {
 	n := maelstrom.NewNode()
@@ -44,13 +29,13 @@ func main() {
 			return err
 		}
 
-		id, err := generateUUIDv4()
+		id, err := uuid.NewV7()
 		if err != nil {
 			return err
 		}
 
 		body["type"] = "generate_ok"
-		body["id"] = id
+		body["id"] = id.String()
 
 		return n.Reply(msg, body)
 	})
