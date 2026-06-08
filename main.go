8c993758bfb7187ec1077b797f9f99ca2839d5c9
Nermin Sehic <sehicnermin97@gmail.com>
2026-06-08 18:21:16 +1000

    Implement unique ID generation

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
+	"fmt"
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
+
+func generateUUIDv4() (string, error) {
+	uuid := make([]byte, 16)
+	_, err := rand.Read(uuid)
+	if err != nil {
+		return "", err
+	}
+
+	uuid[8] = (uuid[8] & 0x3f) | 0x80
+	uuid[6] = (uuid[6] & 0x0f) | 0x40
+
+	return fmt.Sprintf("%x-%x-%x-%x-%x",
+		uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:16]), nil
+}
 
 func main() {
 	n := maelstrom.NewNode()
@@ -17,6 +33,24 @@ func main() {
 		}
 
 		body["type"] = "echo_ok"
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		// Handle generate
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		id, err := generateUUIDv4()
+		if err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+		body["id"] = id
 
 		return n.Reply(msg, body)
 	})
