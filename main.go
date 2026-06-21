54dff2a12036a7469aeafb64f390a1a3b99f4c0c
Nermin Sehic <sehicnermin97@gmail.com>
2026-06-21 15:06:06 +1000

    Challenge #3b: Multi-Node Broadcast

--- main.go
+++ main.go
@@ -9,13 +9,52 @@ import (
 )
 
 type BroadcastRequest struct {
-	Type    string `json:"type'"`
-	Message int    `json:"message"`
+	maelstrom.MessageBody
+	Message int `json:"message"`
+}
+
+type Topology map[string][]string
+
+func (t Topology) NeighborNodes(id string) []string {
+	if t == nil {
+		return nil
+	}
+
+	return t[id]
+}
+
+type TopologyRequest struct {
+	maelstrom.MessageBody
+	Topology Topology `json:"topology"`
+}
+
+type Store struct {
+	messages map[int]struct{}
+	topology Topology
+}
+
+func (s *Store) WriteMessage(msg int) bool {
+	if _, ok := s.messages[msg]; ok {
+		return false
+	}
+	s.messages[msg] = struct{}{}
+	return true
+}
+
+func (s *Store) ReadMessages() []int {
+	var messages []int
+	for msg := range s.messages {
+		messages = append(messages, msg)
+	}
+
+	return messages
 }
 
 func main() {
 	n := maelstrom.NewNode()
-	var messages []int
+	store := &Store{
+		messages: make(map[int]struct{}),
+	}
 
 	n.Handle("echo", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -50,7 +89,16 @@ func main() {
 			return err
 		}
 
-		messages = append(messages, body.Message)
+		// Write message to neighbours only if the message was successfully written to the current node.
+		// We assume that if a node has a message, it's neighbours have the message too.
+		if ok := store.WriteMessage(body.Message); ok {
+			nodes := store.topology.NeighborNodes(n.ID())
+			for _, node := range nodes {
+				if err := n.Send(node, msg.Body); err != nil {
+					log.Printf("Error sending message to %s: %s", node, err)
+				}
+			}
+		}
 
 		return n.Reply(msg, map[string]string{
 			"type": "broadcast_ok",
@@ -58,17 +106,18 @@ func main() {
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		body["type"] = "read_ok"
-		body["messages"] = messages
-		return n.Reply(msg, body)
+		return n.Reply(msg, map[string]any{
+			"type":     "read_ok",
+			"messages": store.ReadMessages(),
+		})
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body TopologyRequest
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		store.topology = body.Topology
 		return n.Reply(msg, map[string]string{
 			"type": "topology_ok",
 		})
